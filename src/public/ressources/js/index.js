2ccb82326be42983f58230b722d1bd66b3172ee3
Yoann Gautier <goannyautier@gmail.com>
2024-11-01 21:59:20 +0100

    ✨ MVP in progress, adding pattern project

--- src/public/ressources/js/index.js
+++ src/public/ressources/js/index.js
@@ -23,16 +23,25 @@ var dicStudyDate = {};
 
 async function writeExperience() {
   try {
-    const response = await fetch('./ressources/js/jsons/experiences.json');
+    const response = await fetch('./ressources/js/jsons/en.experiences.json');
     const data = await response.json();
     // console.log(data);
-    cpt = 1;
+    cpt = 2;
     Array.from(data['experiences']).forEach((element) => {
       // console.log(element);
+      let experience = new Object();
+      writeGenExp(element, experience);
       if (element['category'] == 'job') {
-        writeJob(element);
+        document.getElementById('listExperience').appendChild(experience.vide);
+        document.getElementById('listExperience').appendChild(experience.date);
+        document.getElementById('listExperience').appendChild(experience.description);
+        writeJob(element, experience);
       } else if (element['category'] == 'study') {
-        writeStudy(element, cpt);
+        dicStudyDate[cpt] = experience.date;
+        writeStudy(element, experience);
+        document.getElementById('listExperience').appendChild(experience.date);
+        document.getElementById('listExperience').appendChild(experience.description);
+        document.getElementById('listExperience').appendChild(experience.vide);
       }
       cpt++;
     });
@@ -43,95 +52,64 @@ async function writeExperience() {
     console.error('Error:', error);
   }
 }
+function writeGenExp(element, experience) {
+  experience.date = document.createElement('div');
+  experience.date.classList.add('date');
+  experience.description = document.createElement('div');
+  experience.description.classList.add('description');
+  experience.vide = document.createElement('div');
+  experience.vide.classList.add('vide');
+  let picture = document.createElement('img');
+  picture.setAttribute('src', element['picture']);
+  picture.setAttribute('alt', 'icone de ' + element['title']);
+  experience.description.appendChild(picture);
+  let title = document.createElement('h3');
+  title.textContent = element['title'];
+  experience.description.appendChild(title);
 
-function writeStudy(element, cpt) {
-  let date = document.createElement('div');
-  date.classList.add('date');
-  date.classList.add('dateStudy');
-  dicStudyDate[cpt] = date;
-  let description = document.createElement('div');
-  description.classList.add('description');
-  description.classList.add('descriptionStudy');
-  let vide = document.createElement('div');
-  vide.classList.add('vide');
-  vide.classList.add('videStudy');
+  let firstDate = document.createElement('h3');
+  firstDate.textContent = element['dateBegin'];
+  experience.date.appendChild(firstDate);
+  if (element['dateEnd'] != '') {
+    let dash = document.createElement('h3');
+    dash.textContent = '-';
+    experience.date.appendChild(dash);
+    let secDate = document.createElement('h3');
+    secDate.textContent = element['dateEnd'];
+    experience.date.appendChild(secDate);
+  }
+}
+function writeStudy(element, experience) {
+  experience.date.classList.add('dateStudy');
+  experience.description.classList.add('descriptionStudy');
+  experience.vide.classList.add('videStudy');
 
-  let studyPicture = document.createElement('img');
-  studyPicture.setAttribute('src', element['picture']);
-  studyPicture.setAttribute('alt', 'icone de ' + element['title']);
-  description.appendChild(studyPicture);
-  let studyTitle = document.createElement('h3');
-  studyTitle.textContent = element['title'];
-  description.appendChild(studyTitle);
-  let stydyLine = document.createElement('p');
-  stydyLine.textContent = element['line'];
-  description.appendChild(stydyLine);
+  let studyLine = document.createElement('p');
+  studyLine.textContent = element['line'];
+  experience.description.appendChild(studyLine);
   let studyMention = document.createElement('p');
   studyMention.textContent = element['mention'];
-  description.appendChild(studyMention);
-
-  let studyFirstDate = document.createElement('h3');
-  studyFirstDate.textContent = element['dateBegin'];
-  date.appendChild(studyFirstDate);
-  if (element['dateEnd'] != '') {
-    let dash = document.createElement('h3');
-    dash.textContent = '-';
-    date.appendChild(dash);
-    let studySecDate = document.createElement('h3');
-    studySecDate.textContent = element['dateEnd'];
-    date.appendChild(studySecDate);
-  }
-
-  document.getElementById('listExperience').appendChild(date);
-  document.getElementById('listExperience').appendChild(description);
-  document.getElementById('listExperience').appendChild(vide);
+  experience.description.appendChild(studyMention);
 }
 
-function writeJob(element) {
-  let date = document.createElement('div');
-  date.classList.add('date');
-  date.classList.add('dateJob');
-  let description = document.createElement('div');
-  description.classList.add('description');
-  description.classList.add('descriptionJob');
-  let vide = document.createElement('div');
-  vide.classList.add('vide');
-  vide.classList.add('videJob');
+function writeJob(element, experience) {
+  experience.date.classList.add('dateJob');
+  experience.description.classList.add('descriptionJob');
+  experience.vide.classList.add('videJob');
 
-  let jobPicture = document.createElement('img');
-  jobPicture.setAttribute('src', element['picture']);
-  jobPicture.setAttribute('alt', 'icone de ' + element['title']);
-  description.appendChild(jobPicture);
-  let jobTitle = document.createElement('h3');
-  jobTitle.textContent = element['title'];
-  description.appendChild(jobTitle);
   let jobCompany = document.createElement('p');
   jobCompany.textContent = element['company'];
-  description.appendChild(jobCompany);
+  experience.description.appendChild(jobCompany);
   let jobLocation = document.createElement('p');
   jobLocation.textContent = element['location'];
-  description.appendChild(jobLocation);
-
-  let jobFirstDate = document.createElement('h3');
-  jobFirstDate.textContent = element['dateBegin'];
-  date.appendChild(jobFirstDate);
-  if (element['dateEnd'] != '') {
-    let dash = document.createElement('h3');
-    dash.textContent = '-';
-    date.appendChild(dash);
-    let jobSecDate = document.createElement('h3');
-    jobSecDate.textContent = element['dateEnd'];
-    date.appendChild(jobSecDate);
-  }
-  document.getElementById('listExperience').appendChild(vide);
-  document.getElementById('listExperience').appendChild(date);
-  document.getElementById('listExperience').appendChild(description);
+  experience.description.appendChild(jobLocation);
 }
 
 function gridChange() {
   if (window.innerWidth > 768) {
     for (const key in dicStudyDate) {
       dicStudyDate[key].style.gridRow = key;
+      console.log(dicStudyDate[key]);
     }
   } else {
     for (const key in dicStudyDate) {
@@ -147,3 +125,80 @@ async function test() {
 test();
 // window.onresize = gridChange();
 window.addEventListener('resize', gridChange);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const monObjet = document.getElementById('arrowDown');
+  let aller = true;
+  const deplacementMax = 2.5; // Distance maximale en pixels
+  const deplacementMin = 1.5; // Distance minimale en pixels
+  let bottom = deplacementMin;
+  const vitesse = 0.008; // Ajuster pour changer la vitesse (0.5 = lent, 5 = rapide)
+
+  function animer() {
+    // Calcul de la nouvelle position en fonction de la direction
+    if (aller) {
+      bottom += vitesse;
+      if (bottom >= deplacementMax) {
+        aller = false; // Inverse la direction
+      }
+    } else {
+      bottom -= vitesse * 2;
+      if (bottom <= deplacementMin) {
+        aller = true; // Inverse la direction
+      }
+    }
+
+    // Appliquer la nouvelle position
+    monObjet.style.bottom = `${bottom}rem`;
+
+    // Appelle la prochaine frame
+    requestAnimationFrame(animer);
+  }
+
+  // Lancer l'animation
+  requestAnimationFrame(animer);
+});
+
+document.querySelectorAll('.oneSocialMobile').forEach((element) => {
+  element.addEventListener('mouseenter', () => {
+    element.firstElementChild.setAttribute('fill', 'white');
+  });
+  element.addEventListener('mouseleave', () => {
+    element.firstElementChild.setAttribute('fill', 'black');
+  });
+});
+
+// function writeProjects(){
+//   fetch('./ressources/js/jsons/projects.json')
+//     .then((response) => response.json())
+//     .then((data) => {
+//       // Use the data from the skills.json file here
+//       // console.log(data);
+//       Array.from(data['projects']).forEach((element) => {
+//         // console.log(element);
+//         let project = document.createElement('div');
+//         project.classList.add('project');
+//         let projectTitle = document.createElement('h4');
+//         projectTitle.textContent = element['title'];
+//         project.appendChild(projectTitle);
+//         let projectDescription = document.createElement('p');
+//         projectDescription.textContent = element['description'];
+//         project.appendChild(projectDescription);
+//         let projectLink = document.createElement('a');
+//         projectLink.href = element['link'];
+//         projectLink.textContent = 'Lien vers le projet';
+//         project.appendChild(projectLink);
+//         document.getElementById('listProjects').appendChild(project);
+//       });
+//     })
+//     .catch((error) => {
+//       console.error('Error:', error);
+//     });
+// }
+// writeProjects();
+
+document.getElementsByClassName("project")[0].addEventListener("click", function(){
+  console.log("click");
+  console.log(document.getElementsByClassName("descProject")[0].style.marginTop);
+  document.getElementsByClassName("descProject")[0].style.marginTop = "0rem";
+});
--- src/public/ressources/js/lines.js
+++ src/public/ressources/js/lines.js
@@ -1,12 +1,16 @@
 let canvas = document.getElementById('background_lines');
 let ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth; //<1280?window.innerWidth:1280;
-canvas.height = document.getElementById('background_lines').clientHeight;
-
-const reduc = 0.6;
-var triangleSize = canvas.width / 30;
+canvas.height = document.getElementById('background_lines').clientHeight; //<720?window.innerHeight:720;
+// console.log("canvas.height:"+canvas.height);
+const reduc = 0.55;
+var nbTrinagles = canvas.width < 1000 ? 20 : 50;
+const reducDegrade = 10 / 14;
+const opacity = 0.15;
+var triangleSize = canvas.width / nbTrinagles;
 var halfSize = triangleSize * reduc;
-var rows = Math.ceil(canvas.height / (triangleSize * reduc)) + 1;
+var rows = Math.ceil(canvas.height / triangleSize);
+// console.log("rows:"+rows);
 var cols = Math.ceil(canvas.width / (triangleSize * reduc)) + 1;
 
 const triangles = [];
@@ -30,11 +34,17 @@ function getRandomGray(excludeColor, nuance1, nuance2) {
 
 function createTriangles() {
   let delay = 0;
-  let row ;
+  let row;
+  let degrade = 0;
   for (row = 0; row < rows; row++) {
+    let y = (row * halfSize) / reduc;
+    // console.log("y:"+y);
+    if (row >= rows * reducDegrade) {
+      degrade += (opacity * 100) / (rows - rows * reducDegrade) / 100;
+      // console.log("degrade:"+degrade);
+    }
     for (let col = 0; col < cols; col++) {
       let x = col * halfSize;
-      let y = (row * halfSize) / reduc;
 
       // Déterminer si le triangle pointe vers le haut ou le bas
       let pointingUp = (row + col) % 2 === 0;
@@ -59,17 +69,23 @@ function createTriangles() {
       }
 
       let grayValue = getRandomGray(adjacentColors, 100, 100);
-      triangles.push({ x, y, grayValue, pointingUp, opacity: 0, delay });
+      triangles.push({
+        x,
+        y,
+        grayValue,
+        pointingUp,
+        opacity: 0,
+        delay,
+        degrade,
+      });
       // delay += 1;
     }
-    if(((row * halfSize) / reduc)>=window.innerWidth){
-      break;
-    }
+
     // console.log(window.innerHeight);
   }
-  createTrianglesDegrade(row+1);
+  // createTrianglesDegrade(row+1);
 }
-
+/*
 function createTrianglesDegrade(stoppedRow) {
   let delay = 0;
   let degrade = 100;
@@ -111,7 +127,7 @@ function createTrianglesDegrade(stoppedRow) {
   }
 
 }
-
+*/
 function drawTriangle(x, y, size, color, pointingUp, opacity) {
   ctx.beginPath();
   if (pointingUp) {
@@ -136,9 +152,16 @@ function animateTriangles() {
   triangles.forEach((triangle) => {
     // Vérifier si le temps de delay est passé pour ce triangle
     if (currentTime >= triangle.startTime + triangle.delay) {
-      if (triangle.opacity < 0.15) {
+      // let test = 0.15
+      if (triangle.degrade > 0) {
+        // test = 1;
+      }
+      if (triangle.opacity < opacity - triangle.degrade) {
+        // if (triangle.opacity < test) {
         triangle.opacity += 0.01;
         allStatic = false;
+      } else {
+        // console.log("kk"+triangle.opacity)
       }
       drawTriangle(
         triangle.x,
@@ -168,9 +191,10 @@ animateTriangles();
 window.addEventListener('resize', () => {
   canvas.width = window.innerWidth;
   canvas.height = document.getElementById('background_lines').clientHeight;
-  triangleSize = canvas.width / 10;
+  nbTrinagles = canvas.width < 1000 ? 20 : 50;
+  triangleSize = canvas.width / nbTrinagles;
   halfSize = triangleSize * reduc;
-  rows = Math.ceil(canvas.height / (triangleSize * reduc)) + 1;
+  rows = Math.ceil(canvas.height / triangleSize);
   cols = Math.ceil(canvas.width / (triangleSize * reduc)) + 1;
   triangles.length = 0;
   createTriangles();
@@ -179,6 +203,5 @@ window.addEventListener('resize', () => {
   });
   animateTriangles();
 });
-
 
 //rajouter un dégrader fin lp
