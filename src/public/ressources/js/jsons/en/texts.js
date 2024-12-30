3eb0e519449dfbfd8e0ea8a01a90025cb1c490f2
TMLG\YGA23871 <YGA23871@open-groupe.com>
2024-12-30 11:44:53 +0100

    ⚡️translate all in french

---
+++ src/public/ressources/js/jsons/en/texts.js
@@ -0,0 +1,6 @@
+[{
+    "about": "Passionate about programming for more than 4 years, I trained myself thanks to OpenClassroom, then I joined a BUT Informatique after my general baccalaureate in 2022. I carried out numerous projects, whether personal or related to my studies, which gradually brought me to my current level. During these, I enjoyed overcoming the difficulties encountered, exploring many options and ensuring that the project was completely finished so that I could be proud of the result."
+},
+{
+    "landing": "<h1>Hi, I'm <span>Yoann GAUTIER</span><br />A <span>student</span> in<br /><span class=\"landingBig\">Software</span><br /><span class=\"landingBig\">Developpement</span></h1>"
+}]
